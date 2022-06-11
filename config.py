bde0b462530266dfc7e5c5dc09f93178820608de
SnowLire <65495751+SnowLire@users.noreply.github.com>
2022-06-11 08:04:30 +0200

    more formatting

--- config.py
+++ config.py
@@ -55,8 +55,10 @@ screens = [
             ],
             24,
             background="#222222",
-            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-            # border_color=['ff00ff', '000000', 'ff00ff', '000000']  # Borders are magenta
+            # Draw top and bottom borders
+            # border_width=[2, 0, 2, 0],
+            # Borders are magenta
+            # border_color=['ff00ff', '000000', 'ff00ff', '000000']
         ),
     ),
 ]
